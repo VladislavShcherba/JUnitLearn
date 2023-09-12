8735c0d007092c418cafe9e8fa03076eecbf28c0
Vladislav Shcherba <zerik105@gmail.com>
2023-09-12 18:43:55 +0400

    Add more examples with @Tag

--- src/test/java/p02WritingTests/e05Tags/Tags.java
+++ src/test/java/p02WritingTests/e05Tags/Tags.java
@@ -3,10 +3,63 @@ package p02WritingTests.e05Tags;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-@Tag("ClassTag")
-class TaggedTest {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("Smoke")
+@interface Smoke {
+}
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("Regression")
+@interface Regression {
+}
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("Online")
+@interface Online {
+}
+
+@Smoke
+class SmokeTest {
 
     @Test
-    @Tag("MethodTag")
-    void test(){}
+    void smokeOffline() {
+        System.out.println("smokeOffline()");
+    }
+
+    @Online
+    @Test
+    void smokeOnline() {
+        System.out.println("smokeOnline()");
+    }
+}
+
+@Regression
+class RegressionTest {
+
+    @Test
+    void regressionOffline() {
+        System.out.println("regressionOffline()");
+    }
+
+    @Online
+    @Test
+    void regressionOnLine() {
+        System.out.println("regressionOnline()");
+    }
+}
+
+class OtherTest {
+
+    @Test
+    void otherTest() {
+        System.out.println("otherTest()");
+    }
 }
---
+++ src/test/java/p04RunningTests/e06Tags/TagsTest.java
@@ -0,0 +1,8 @@
+package p04RunningTests.e06Tags;
+
+class TagsTest {
+    //see p02WritingTests.e05Tags
+    //examples:
+    //mvn test -Dgroups="Smoke & Online"
+    //mvn test -Dgroups="Regression & !Online"
+}
